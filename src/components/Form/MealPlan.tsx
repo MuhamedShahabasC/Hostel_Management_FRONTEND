3d820a2df9923706cdb57a116c3deb1c47bec65e
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-04-09 13:00:58 +0530

    Profile for staff

--- src/components/Form/MealPlan.tsx
+++ src/components/Form/MealPlan.tsx
@@ -1,123 +1,123 @@
-import { useState } from "react";
-import { addNewPlan, updateMealPlan } from "../../apiRoutes/staff";
-import Input from "../../components/Form/Input";
-import { Form, Formik } from "formik";
-import LoadingButton from "../../components/UI/LoadingButton";
-import Button from "../../components/UI/Button";
-import { toast } from "react-toastify";
-import { mealPlanSchema } from "../../schema/staff";
-import { IMealPlan } from "../../interfaces/chef";
+// import { useState } from "react";
+// import { addNewPlan, updateMealPlan } from "../../apiRoutes/staff";
+// import Input from "../../components/Form/Input";
+// import { Form, Formik } from "formik";
+// import LoadingButton from "../../components/UI/LoadingButton";
+// import Button from "../../components/UI/Button";
+// import { toast } from "react-toastify";
+// import { mealPlanSchema } from "../../schema/staff";
+// import { IMealPlan } from "../../interfaces/chef";
 
-function MealPlanForm({ modalData, role, fetchAllMeals, setModal }: FormRole) {
-  const [message, setMessage] = useState<string | null>(null);
+// function MealPlanForm({ modalData, role, fetchAllMeals, setModal }: FormRole) {
+//   const [message, setMessage] = useState<string | null>(null);
 
-  const submitHandler = async (formData: IMealPlan, _id?: string) => {
-    if (role === "edit" && _id) {
-      return await updateMealPlan(_id, formData);
-    } else if (role === "new") {
-      return await addNewPlan(formData);
-    }
-  };
+//   const submitHandler = async (formData: IMealPlan, _id?: string) => {
+//     if (role === "edit" && _id) {
+//       return await updateMealPlan(_id, formData);
+//     } else if (role === "new") {
+//       return await addNewPlan(formData);
+//     }
+//   };
 
-  return (
-    <>
-      <Formik
-        enableReinitialize
-        initialValues={{
-          title: modalData?.title,
-          price: modalData?.price,
-          breakfast: modalData?.breakfast,
-          lunch: modalData?.lunch,
-          evening: modalData?.evening,
-          dinner: modalData?.dinner,
-        }}
-        validationSchema={mealPlanSchema}
-        onSubmit={(formData, { setSubmitting }) => {
-          setSubmitting(true);
-          submitHandler(formData, modalData._id)
-            .then(({ data: { message } }: any) => {
-              fetchAllMeals();
-              toast.success(message);
-              setModal(false);
-            })
-            .catch(
-              ({
-                response: {
-                  data: { message },
-                },
-              }) => setMessage(message)
-            )
-            .finally(() => setSubmitting(false));
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className="flex flex-col justify-center gap-4 px-1 mb-3">
-            <Input
-              type="text"
-              placeholder="Title"
-              name="title"
-              id="title"
-              edit
-            />
-            <Input
-              type="number"
-              placeholder="Price"
-              name="price"
-              id="price"
-              edit
-            />
-            <Input
-              type="text"
-              placeholder="Breakfast"
-              name="breakfast"
-              id="breakfast"
-              edit
-            />
-            <Input
-              type="text"
-              placeholder="Lunch"
-              name="lunch"
-              id="lunch"
-              edit
-            />
-            <Input
-              type="text"
-              placeholder="Evening"
-              name="evening"
-              id="evening"
-              edit
-            />
-            <Input
-              type="text"
-              placeholder="Dinner"
-              name="dinner"
-              id="dinner"
-              edit
-            />
-            {isSubmitting ? (
-              <LoadingButton />
-            ) : (
-              <Button className="max-w-fit mx-auto" type="submit">
-                Save Changes
-              </Button>
-            )}
-          </Form>
-        )}
-      </Formik>
-      {message && (
-        <span className="text-center text-md font-semibold text-red-700">
-          {message}
-        </span>
-      )}
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Formik
+//         enableReinitialize
+//         initialValues={{
+//           title: modalData?.title,
+//           price: modalData?.price,
+//           breakfast: modalData?.breakfast,
+//           lunch: modalData?.lunch,
+//           evening: modalData?.evening,
+//           dinner: modalData?.dinner,
+//         }}
+//         validationSchema={mealPlanSchema}
+//         onSubmit={(formData, { setSubmitting }) => {
+//           setSubmitting(true);
+//           submitHandler(formData, modalData._id)
+//             .then(({ data: { message } }: any) => {
+//               fetchAllMeals();
+//               toast.success(message);
+//               setModal(false);
+//             })
+//             .catch(
+//               ({
+//                 response: {
+//                   data: { message },
+//                 },
+//               }) => setMessage(message)
+//             )
+//             .finally(() => setSubmitting(false));
+//         }}
+//       >
+//         {({ isSubmitting }) => (
+//           <Form className="flex flex-col justify-center gap-4 px-1 mb-3">
+//             <Input
+//               type="text"
+//               placeholder="Title"
+//               name="title"
+//               id="title"
+//               edit
+//             />
+//             <Input
+//               type="number"
+//               placeholder="Price"
+//               name="price"
+//               id="price"
+//               edit
+//             />
+//             <Input
+//               type="text"
+//               placeholder="Breakfast"
+//               name="breakfast"
+//               id="breakfast"
+//               edit
+//             />
+//             <Input
+//               type="text"
+//               placeholder="Lunch"
+//               name="lunch"
+//               id="lunch"
+//               edit
+//             />
+//             <Input
+//               type="text"
+//               placeholder="Evening"
+//               name="evening"
+//               id="evening"
+//               edit
+//             />
+//             <Input
+//               type="text"
+//               placeholder="Dinner"
+//               name="dinner"
+//               id="dinner"
+//               edit
+//             />
+//             {isSubmitting ? (
+//               <LoadingButton />
+//             ) : (
+//               <Button className="max-w-fit mx-auto" type="submit">
+//                 Save Changes
+//               </Button>
+//             )}
+//           </Form>
+//         )}
+//       </Formik>
+//       {message && (
+//         <span className="text-center text-md font-semibold text-red-700">
+//           {message}
+//         </span>
+//       )}
+//     </>
+//   );
+// }
 
-export default MealPlanForm;
+// export default MealPlanForm;
 
-interface FormRole {
-  modalData: IMealPlan;
-  role: "edit" | "new";
-  fetchAllMeals: () => any;
-  setModal: (state: boolean) => void;
-}
+// interface FormRole {
+//   modalData: IMealPlan;
+//   role: "edit" | "new";
+//   fetchAllMeals: () => any;
+//   setModal: (state: boolean) => void;
+// }
